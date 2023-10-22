06ad98f8d7981f5144e0d650ab888edb2c8f0dc4
zenith-78 <anshumankhatua78@gmail.com>
2023-10-22 16:23:45 +0530

    added testing file and moddified  password generator

--- PassWordGenerator.py
+++ PassWordGenerator.py
@@ -40,4 +40,4 @@ has_number = input("Do you want to have numbers(y/n)").lower() == 'y'
 has_special = input("Do you want to have special Characters(y/n)").lower() == 'y'
 
 pwd = generate_password(min_length, has_number, has_special)
-print("The generated password is", pwd)
+print("The generated password is", pwd) 
